ef0930f383b4bbfb961f0025f6162db19f5e61b3
Fernando200519 <zs23021811@estudiantes.uv.mx>
2025-05-05 18:46:30 -0600

    Front del login

--- src/main/main.js
+++ src/main/main.js
@@ -12,7 +12,7 @@ function createWindow() {
   });
   // mainWindow.setMenu(null);
   mainWindow.maximize();
-  mainWindow.loadFile(path.join(__dirname, '../renderer/alumno/alumno.html'));
+  mainWindow.loadFile(path.join(__dirname, '../renderer/login/login.html'));
 }
 
 app.whenReady().then(createWindow);
--- src/renderer/login/login.js
+++ src/renderer/login/login.js
@@ -1,8 +1,55 @@
 const passwordInput = document.getElementById('contraseña');
 const togglePassword = document.getElementById('toggle-password');
+const form = document.querySelector('.login-form');
+const mensajeError = document.getElementById('mensaje-error');
 
 togglePassword.addEventListener('click', () => {
   const isPassword = passwordInput.type === 'password';
   passwordInput.type = isPassword ? 'text' : 'password';
   togglePassword.src = isPassword ? '../../../assets/mostrar.png' : '../../../assets/esconder.png';
+});
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const usuario = document.getElementById('usuario').value.trim();
+  const contraseña = passwordInput.value.trim();
+
+  if (usuario === '' || contraseña === '') {
+    mensajeError.textContent = 'Por favor, llena todos los campos.';
+    mensajeError.style.display = 'block';
+    return;
+  }
+
+  
+  /*
+  try {
+    const response = await fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ usuario, contraseña })
+    });
+
+    const result = await response.json();
+
+    if (result.exito) {
+      if (result.tipoUsuario === 'alumno') {
+        window.location.href = '../alumno/alumno.html';
+      } else if (result.tipoUsuario === 'asesor') {
+        window.location.href = '../alumno/asesor.html';
+      } else {
+        mensajeError.textContent = 'Tipo de usuario desconocido.';
+        mensajeError.style.display = 'block';
+      }
+    } else {
+      mensajeError.textContent = 'Usuario o contraseña incorrectos.';
+      mensajeError.style.display = 'block';
+    }
+  } catch (error) {
+    mensajeError.textContent = 'Error al conectar con el servidor.';
+    mensajeError.style.display = 'block';
+    console.error(error);
+  }
+    */
+
 });
