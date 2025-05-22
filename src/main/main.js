1a32a15f14590a365a2ab76a973017c4d230d741
Fernando200519 <zs23021811@estudiantes.uv.mx>
2025-05-22 00:13:31 -0600

    Ajustando estructura

---
+++ src/main/main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+  });
+  // mainWindow.setMenu(null);
+  mainWindow.maximize();
+  mainWindow.loadFile(path.join(__dirname, '../renderer/asesor/index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
--- src/renderer/asesor/preload.js
+++ src/renderer/asesor/preload.js
@@ -4,4 +4,3 @@ const { contextBridge } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   // Tus APIs seguras aquí
 });
-
